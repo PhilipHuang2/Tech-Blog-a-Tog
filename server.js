39e541c91852976d196aa795485d732e14dc42c2
Philip Huang <philiphuang2@gmail.com>
2022-11-06 18:56:31 -0800

    added empty listening server with a database tech_db

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequalizeStore = require('connect-session-sequelize')(session.Store);
+
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequalizeStore({
+      db: sequelize,
+    }),
+  };
+
+app.use(session(sess));
+
+//This is where you can add helpers to to templates
+const hbs = exphbs.create({ helpers });
+  
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+  
+// app.use(routes);
+  
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
