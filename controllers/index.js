ae7895355169ed218eac5eca97cb67f96cfc2d29
Philip Huang <philiphuang2@gmail.com>
2022-11-07 19:18:31 -0800

    added main handlebar to a okay status

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 // HomePage localhost:3001/ ...
 router.get('/', (req,res) => {
-    res.render('homepage');
+    res.render('homepage', {loggedIn: req.session.loggedIn});
 })
 
 module.exports = router;
