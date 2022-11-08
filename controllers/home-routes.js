25fd331d5a4b08067dd49d7a20c4e7e40ec8a063
Philip Huang <philiphuang2@gmail.com>
2022-11-07 21:23:06 -0800

    migrated homeRoutes to its own file

---
+++ controllers/home-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Sequelize } = require('sequelize');
+const {Post, User, Comment} = require('../models')
+
+// HomePage localhost:3001/ ...
+router.get('/', async (req,res) => {
+    const postData = await Post.findAll(
+        {
+            attributes: ['title', 'content', 'post_date', [Sequelize.col('user.name'), 'name']], 
+            include: [
+                {
+                    model: User, 
+                }
+            ],
+            limit: 5,
+    });
+    const posts = postData.map((element)=> {
+        // console.log("\n\n\nthis is a new element ")
+        // console.log(element.dataValues);
+        return element.get({plain:true})
+    });
+    // console.log(postData);
+    console.log(posts);
+    res.render('homepage', {loggedIn: req.session.loggedIn, posts: posts});
+})
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,29 +1,7 @@
 const router = require('express').Router();
-const { Sequelize } = require('sequelize');
-const sequelize = require('../config/connection');
-// const { Model } = require('sequelize');
-const {Post, User, Comment} = require('../models')
+const homeRoutes = require('./home-routes')
 
 
-// HomePage localhost:3001/ ...
-router.get('/', async (req,res) => {
-    const postData = await Post.findAll(
-        {
-            attributes: ['title', 'content', 'post_date', [Sequelize.col('user.name'), 'name']], 
-            include: [
-                {
-                    model: User, 
-                }
-            ],
-    });
-    const posts = postData.map((element)=> {
-        // console.log("\n\n\nthis is a new element ")
-        // console.log(element.dataValues);
-        return element.get({plain:true})
-    });
-    // console.log(postData);
-    console.log(posts);
-    res.render('homepage', {loggedIn: req.session.loggedIn, posts: posts});
-})
+router.use('/', homeRoutes);
 
 module.exports = router;
