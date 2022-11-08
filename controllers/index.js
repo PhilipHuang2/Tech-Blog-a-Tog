5c70afed7fd69bc847f1e6fb8f6d29a1836021e9
Philip Huang <philiphuang2@gmail.com>
2022-11-07 20:14:21 -0800

    finalized database pull for homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
-
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+// const { Model } = require('sequelize');
+const {Post, User, Comment} = require('../models')
 
 
 // HomePage localhost:3001/ ...
-router.get('/', (req,res) => {
+router.get('/', async (req,res) => {
+    const postData = await Post.findAll(
+        {
+            attributes: ['title', 'content', 'post_date', [Sequelize.col('user.name'), 'name']], 
+            include: [
+                {
+                    model: User,  
+                    attributes: ['name']
+
+                }
+            ],
+    });
+    const posts = postData.map((element)=> {
+        // console.log("\n\n\nthis is a new element ")
+        // console.log(element.dataValues);
+        return element.get({plain:true})
+    });
+    // console.log(postData);
+    console.log(posts);
     res.render('homepage', {loggedIn: req.session.loggedIn});
 })
 
