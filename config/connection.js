1dec0cddbaad6be7800da9603e135546f12d0f77
Philip Huang <philiphuang2@gmail.com>
2022-11-08 23:06:46 -0800

    added seqaulize2

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
+let sequelize;
 require('dotenv').config();
+if(process.env.JAWSDB_URL)
+{
+  const seqaulize2 = new Sequelize( process.env.JAWSDB_URL) 
+    module.exports = seqaulize2;
+   sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host:'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host:'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+
 
 module.exports = sequelize;
